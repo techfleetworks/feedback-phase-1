5f07908caad7701aa84e943b9869d43d55f6cf32
cjwe <christajwest@gmail.com>
2022-07-05 17:59:46 +0200

    Comment out local host

--- graphql/apollo.tsx
+++ graphql/apollo.tsx
@@ -1,7 +1,8 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { PropsWithChildren } from 'react';
-
-const GRAPHQL_ENDPOINT = 'http://localhost:1337/graphql';
+// Uncomment to run locally
+// const GRAPHQL_ENDPOINT = 'http://localhost:1337/graphql';
+const GRAPHQL_ENDPOINT = 'https://tranquil-journey-42623.herokuapp.com/graphql';
 
 // Creates Apollo client with reference to client
 const StrapiApolloProvider: React.FC<PropsWithChildren<{}>> = ({
