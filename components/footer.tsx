dc0b6bc6ad4697076a427e99d959d27a4a70cb47
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-07-07 10:07:29 -0700

    basic footer skeleton

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import Link from "next/link";
 
 type Props = {}
 
 const footer = (props: Props) => {
   return (
     <>
-        <footer>
-            <div>I'm the footer!</div>
+        <footer className="grid grid-cols-3 w-full py-4">
+          <div>
+            <h5>Mini Sitemap</h5>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="#">Login</Link>
+              </li>
+              <li>
+                <Link href="#">FAQs/Troubleshooting</Link>
+              </li>
+            </ul>
+          </div>
+          <div>Socials/Blog Link</div>
+          <div>
+            <h5>Contact Us</h5>
+            <a href="#">Email</a>
+          </div>
         </footer>
     </>
   )
-}
+};
 
 export default footer;
