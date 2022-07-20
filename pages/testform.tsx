7847ce8e9a69e10cd93cd38e7f66eb0e189efe3d
dcampbellcreative <dougcampbell1990@gmail.com>
2022-07-20 11:26:23 -0400

    update test form

---
+++ pages/testform.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+
+type Props = {};
+
+import { TestAPIService } from "../services/TestAPI";
+
+const Testform = (props: Props) => {
+  const [name, setName] = useState("");
+
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+
+    // if (!user) {
+    //   alert("Please add your username");
+    //   return;
+    // }
+
+    await TestAPIService.create({
+      data: {
+        name: name,
+      },
+    });
+
+    await setName("");
+  };
+
+  return (
+    <div>
+      <div className="inline-flex mb-3">
+        <label className="w-12">Name</label>
+        <input className="border-2 w-25" type="text" name="name"></input>
+      </div>
+
+      <div>
+        <label>Boolean</label>
+        <select>
+          <option>True</option>
+          <option>False</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Testform;
