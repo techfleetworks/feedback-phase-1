6382ea775411e449edadddd9170d122ada086c7d
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-11-10 17:08:18 -0800

    homepage components creation

---
+++ components/HowItWorksSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type Props = {}
+const HowItWorksSection = (props: Props) => {
+  return (
+    <div className='h-[calc(100vh_*_1.5)] bg-[#EEE4BA]'>
+        <h1 className='text-7xl font-anton text-[#2E6756] absolute right-10 mt-5'>How it works</h1>
+    </div>
+  )
+}
+
+export default HowItWorksSection;
---
+++ components/PricingSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type Props = {}
+const PricingSection = (props: Props) => {
+  return (
+    <div className='h-screen bg-[#EBB36C]'>
+
+    </div>
+  )
+}
+
+export default PricingSection;
