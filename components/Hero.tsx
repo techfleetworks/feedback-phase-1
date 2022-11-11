28f25650380b098ad7eac946768aef1c07b17789
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-11-10 16:06:37 -0800

    landing component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,24 +4,26 @@ type Props = {};
 
 const Hero = (props: Props) => {
   return (
-    <div className="flex flex-1 flex-col w-2/3 m-auto my-5">
-      <div>
-        <h1 className="uppercase text-2xl text-center my-5 py-5">
-          Feedback Tagline
-        </h1>
-        <p className="text-justify text-sm mb-4">
-          Feedback is a community of values-based food producers and evaluators
-          who are committed to making our foodways more ethical, equitable, and
-          sustainable. Feedback connects producers to the evaluators (retailers
-          and distributors across the country) that can provide answers about
-          how their products are perceived.
-        </p>
+    <div>
+      <div className='h-[36rem] text-center pt-48 bg-zinc-700'>
+        video content here
       </div>
-      <div className="aspect-video my-5 bg-slate-200 rounded-sm text-center">
-        embedded video goes here
+      <div className='bg-gradient-to-b from-[#151515] to-[#343434] h-[22rem] pl-14'>
+        {/* <div className="aspect-video bg-slate-200 rounded-sm text-center">
+        </div> */}
+        <p className="font-anton text-8xl text-[#EED153] text-left">
+          Get Feedback
+        </p>
+        <p className="font-anton text-8xl text-[#EED153] text-left">
+          Join the community
+        </p>
+        <p className="text-justify text-[#EED153] text-md max-w-[952] w-3/4">
+          Feedback is a community of values-based food producers and evaluators who are committed to making our foodways more ethical, equitable, and sustainable. Feedback connects producers to the evaluators (retailers and distributors across the country) that can provide answers about how their products are perceived.
+        </p>
       </div>
     </div>
   );
 };
+
 
 export default Hero;
